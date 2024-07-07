abb7f1359bbcd6ad8d54a9daa7e02afa28f79118
Rodrigo C <rodrigocapre@gmail.com>
2024-07-07 15:53:48 -0300

    v2

--- src/script.js
+++ src/script.js
@@ -1,2 +1,22 @@
 //Api info https://developer.themoviedb.org/docs/getting-started
 apiTMBDtoken='eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzNjFjMjRlMzRhOTVlODcyMzM3Zjg1ZTIwZTA0NjVjNCIsIm5iZiI6MTcyMDE5NzUzNS4yOTA4OSwic3ViIjoiNjY4ODAzOTJmNGJiMjQ5OGYzYjBhMmFlIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.hJ6wjC2I_Uj_k3Kg5lk65vJ3cLH-Mh7PSgq5U3g6LTk'
+const ActorName='Tom Cruise'
+var ActorId='';
+
+const getActorId = (async () => { //funcion para obtener el id del actor
+  var authParameters = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer ' + apiTMBDtoken
+    }
+  };
+  await fetch(`https://api.themoviedb.org/3/search/person?query=${ActorName}`,authParameters)
+    .then(result => result.json())
+    .then(data =>{
+      respuesta = data.results[0].id; 
+      console.log(data);
+  });
+  ActorId=respuesta;
+})(); //Se va a ejecutar primero, devuelve el id para buscar peliculas en tmdb
+
